da06cdbb0d91bf38f13a9466b21e76b58e637528
close852 <close852@naver.com>
2019-07-24 17:37:27 +0900

    css 스타일 좀 더 손보기 & index > Root > App 순서로 구성.
    * 다음은 게시판?

--- card-search-frontend/src/App.js
+++ card-search-frontend/src/App.js
@@ -1,11 +1,12 @@
 
 import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom';
-import Imsi from './Imsi'
-import Imsi2 from './Imsi2'
 import { List } from 'immutable'
 import { Header, Footer, Remocon } from 'layout'
 import { TodoList } from 'components/todo'
+import { ProductAdd } from 'components/product'
+import axios from 'axios'
+import './App.css'
 class App extends Component {
 
     state = {
@@ -16,6 +17,59 @@ class App extends Component {
             todoList: List(todoList)
         })
     }
+
+
+    componentDidMount() {
+        // console.log('this.props.location', this.props, queryString);
+        axios.get('/api/todo')
+            .then(res => {
+                this.setState({
+                    todoList: List(res.data)
+                })
+            })
+        console.log("init...!!", this.state.cardList)
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        const prevCardList = this.state.todoList;
+        const nextCardList = nextState.todoList;
+        console.log("shouldComponentUpdate >>", prevCardList, nextCardList)
+        let isIdentity = this.identityComp(prevCardList, nextCardList);
+        console.log('shouldComponentUpdate', prevCardList.size, nextCardList.size, isIdentity);
+        return isIdentity;
+    }
+    // List, List
+    //기준 : 1. size 같고, 2. id 순서 동일
+    identityComp(prevCardList, nextCardList) {
+        let checkValid = false;
+        if (prevCardList.size !== nextCardList.size) {
+            return true;
+        }
+
+        prevCardList.forEach((item, idx, array) => {
+            const nextCard = nextCardList.get(idx);
+            // console.log(item, idx, array)
+            // card의 ID가 다르면 rerender
+            console.log("id compare ", item.id, nextCard.id)
+            if (item.id !== nextCard.id) {
+                checkValid = true;
+                return;
+            } else {
+                // card Id가 같을 때 tagList[array]의 tag ID가 다르면 rerender
+                if (item.tagList) {
+                    item.tagList.forEach((item, idx, arr) => {
+                        if (item.id !== nextCard.tagList[idx].id) {
+                            checkValid = true;
+                            return;
+                        }
+                    })
+                }
+            }
+        });
+
+        return checkValid;
+    }
+
     render() {
         const { setTodoList } = this;
         const { todoList } = this.state;
@@ -23,11 +77,13 @@ class App extends Component {
             <div>
                 <Remocon />
                 <Route path="/" component={({ history, location }) => (<Header setTodoList={setTodoList} history={history} location={location} />)} />
-                <Switch>
-                    <Route path="/" component={() => (<TodoList cardList={todoList} setTodoList={setTodoList} />)} />
-                    <Route path="/search" component={() => (<TodoList cardList={todoList} setTodoList={setTodoList} />)} />
-                    <Route path="/product/add" component={Imsi2} />
-                </Switch>
+                <div className="container-wrapper">
+                    <Switch>
+                        <Route exact path="/" component={() => (<TodoList cardList={todoList} setTodoList={setTodoList} />)} />
+                        <Route exact path="/search" component={() => (<TodoList cardList={todoList} setTodoList={setTodoList} />)} />
+                        <Route exact path="/product/add" component={ProductAdd} />
+                    </Switch>
+                </div>
                 <Route path="/" component={Footer} />
 
             </div>
