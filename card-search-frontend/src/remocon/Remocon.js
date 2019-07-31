e0a23fc0789a6e1f34a866c701ca8c977e45bc85
close852 <close852@naver.com>
2019-07-31 17:02:31 +0900

    remote Link로 변경

--- card-search-frontend/src/remocon/Remocon.js
+++ card-search-frontend/src/remocon/Remocon.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import './Remocon.css'
 function Remocon() {
     return (
         <div className="remote" >
             <div>
-                <Button className="button" size="lg" >T</Button>
+                <Button className="button btnStyles" size="lg" >T</Button>
             </div>
             <div>
-                <Button className="button" size="lg" href="/product/add">+</Button>
+
+                <Button className="button" size="lg" ><Link to="/product/add" className="btnStyles">+</Link></Button>
+
             </div>
             <div>
-                <Button className="button" size="lg" href="/">H</Button>
+
+                <Button className="button" size="lg" href="/"><Link to="/" className="btnStyles">H</Link></Button>
+
             </div>
         </div>
     )
